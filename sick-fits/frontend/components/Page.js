97533a2724506ae05d43f1e285a9ad21b905eb30
Jamie Gobeille <djent34@gmail.com>
2020-09-02 17:30:37 +0900

    first commit. set up basic pages of website

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+import Header from './Header';
+import Meta from './Meta';
+
+class Page extends Component {
+  render() {
+    const { children } = this.props;
+    return (
+      <div>
+        <Meta />
+        <Header />
+        {children}
+      </div>
+    );
+  }
+}
+
+export default Page;
